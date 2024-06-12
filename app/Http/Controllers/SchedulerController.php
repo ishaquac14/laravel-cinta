585133f9f781ae173f4ee4a8898344c4145c4a11
Rohmat Maulana Ishaq <rohmat@aiia.co.id>
2024-06-12 14:43:52 +0700

    alert whatsapp foreach

--- app/Http/Controllers/SchedulerController.php
+++ app/Http/Controllers/SchedulerController.php
@@ -25,7 +25,7 @@ class SchedulerController extends Controller
     }
 
     public function runScheduler()
-    {   
+    {
         $now = Carbon::now();
         $day = $now->day;
         $yesterday = Carbon::yesterday()->toDateString();
@@ -46,22 +46,22 @@ class SchedulerController extends Controller
         if ($yesterday_acservers == 0) {
             $messages[] = "Hari ini Checksheet AC SERVER tidak diisi!";
         }
-            
+
         if ($yesterday_cctvs == 0) {
             $messages[] = "Hari ini Checksheet CCTV tidak diisi!";
-         }
+        }
 
         if ($yesterday_csdatabases == 0) {
             $messages[] = "Hari ini Checksheet Database tidak diisi!";
-         }
+        }
 
         if ($yesterday_cserverelectrics == 0) {
             $messages[] = "Hari ini Checksheet Server Electric tidak diisi!";
-         }
+        }
 
         if ($yesterday_fujixeroxs == 0) {
             $messages[] = "Hari ini Checksheet Printer Fujixerox tidak diisi!";
-         }
+        }
 
         if ($yesterday_gacsirts == 0) {
             $messages[] = "Hari ini Checksheet GA-CSIRT tidak diisi!";
@@ -78,36 +78,34 @@ class SchedulerController extends Controller
         if ($yesterday_tapedrives == 0) {
             $messages[] = "Hari ini Checksheet Tape Drive tidak diisi!";
         }
-        
+
         if (!empty($messages)) {
-             $nomor = ['081223506433', '082125008160', '085282716716', '082260050066', '082111707754'];
-             $isi = "WARNING !!!\n\n" . implode("\n", $messages);
+            $nomors = ['081223506433', '082125008160', '085282716716', '082260050066', '082111707754'];
+            $isi = "WARNING !!!\n\n" . implode("\n", $messages);
 
-        $token = "v2n49drKeWNoRDN4jgqcdsR8a6bcochcmk6YphL6vLcCpRZdV1";
-        $message = sprintf("------------CINTA------------%c$isi%c-------------------------------- ", 10, 10);
+            foreach ($nomors as $nomor) {
+                $token = "v2n49drKeWNoRDN4jgqcdsR8a6bcochcmk6YphL6vLcCpRZdV1";
+                $message = sprintf("------------CINTA------------%c$isi%c-------------------------------- ", 10, 10);
 
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-              CURLOPT_URL => 'https://app.ruangwa.id/api/send_message',
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => 'token=' . $token . '&number=' . $nomor[0] . '&message=' . $message,
-        ));
-        $response = curl_exec($curl);
-        curl_close($curl);
+                $curl = curl_init();
+                curl_setopt_array($curl, array(
+                    CURLOPT_URL => 'https://app.ruangwa.id/api/send_message',
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => '',
+                    CURLOPT_MAXREDIRS => 10,
+                    CURLOPT_TIMEOUT => 0,
+                    CURLOPT_FOLLOWLOCATION => true,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => 'POST',
+                    CURLOPT_POSTFIELDS => 'token=' . $token . '&number=' . $nomor . '&message=' . $message,
+                ));
+                $response = curl_exec($curl);
+                curl_close($curl);
+            }
         }
-        // Artisan::call('schedule:run');
-        // return response()->json(['message' => 'Scheduler run successfully']);
     }
 
     public function checkAcserver()
     {
-
     }
 }
-
