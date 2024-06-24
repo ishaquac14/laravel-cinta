e7139d4dee00366ec689a9cde4534f392b94ee6a
Rohmat Maulana Ishaq <rohmat@aiia.co.id>
2024-06-25 01:02:57 +0700

    update login

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -15,60 +15,64 @@
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
-<body class="bg-gradient-primary" style="background-color: rgb(34, 34, 106);">
-    <div class="row justify-content-center mt-5">
-        <div class="card col-lg-3 mt-5">
+<body class="bg-gradient-primary d-flex align-items-center justify-content-center vh-100" style="background-color: rgb(34, 34, 106);">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="card col-lg-4 col-md-8 col-sm-10">
 
-            @if (session('success'))
-                <div class="alert alert-success alert-dismissible fade show" role="alert">
-                    {{ session('success') }}
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
-            @endif
-
-            @if (session('loginError'))
-                <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                    {{ session('loginError') }}
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
-            @endif
-
-            <main class="form-signin w-100 m-auto">
-                <form action="/login" method="post">
-                    @csrf
-                    <div class="mt-3 mb-0 text-center">
-                        <img src="/images/aisin.png" height="50px" style="margin-bottom: 40px;">
+                @if (session('success'))
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        {{ session('success') }}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                     </div>
+                @endif
 
-                    <div class="form-floating mt-0">
-                        <input type="email" name="email" class="form-control" id="email"
-                            placeholder="name@example.com" autofocus required>
-                        <label for="email">Email address</label>
+                @if (session('loginError'))
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        {{ session('loginError') }}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                     </div>
-                    <div class="form-floating">
-                        <input type="password" name="password" class="form-control @error('email') is-invalid @enderror"
-                            id="password" placeholder="Password" value="{{ old('email') }}" required>
-                        <label for="password">Password</label>
-                        @error('email')
-                            <div class="feedback">
-                                {{ $message }}
-                            </div>
-                        @enderror
-                    </div>
+                @endif
 
-                    <div class="form-check text-start my-3">
+                <main class="form-signin w-100 m-auto">
+                    <form action="/login" method="post">
+                        @csrf
+                        <div class="mt-3 mb-0 text-center">
+                            <img src="/images/aisin.png" height="50px" style="margin-bottom: 40px;">
+                        </div>
+
+                        <div class="form-floating mt-0">
+                            <input type="email" name="email" class="form-control" id="email"
+                                placeholder="name@example.com" autofocus required>
+                            <label for="email">Email address</label>
+                        </div>
+                        <div class="form-floating">
+                            <input type="password" name="password"
+                                class="form-control @error('email') is-invalid @enderror" id="password"
+                                placeholder="Password" value="{{ old('email') }}" required>
+                            <label for="password">Password</label>
+                            @error('email')
+                                <div class="feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        <br>
+
+                        {{-- <div class="form-check text-start my-3">
                         <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
                         <label class="form-check-label" for="flexCheckDefault">
                             Remember me
                         </label>
-                    </div>
-                    <button class="btn btn-primary w-100 py-2" type="submit">Login</button>
-                    <div class="mt-3 text-center mb-4">
-                        <small class="mt-3 mb-3 text-body-secondary">&copy;2023-ITD | Not Registered? <a
-                                href="/register">Register Now!</a></small>
-                    </div>
-                </form>
-            </main>
+                    </div> --}}
+                        <button class="btn btn-primary w-100 py-2" type="submit">Login</button>
+                        <div class="mt-4 text-center mb-1">
+                            <small class="mt-3 mb-3 text-body-secondary">&copy;2023-IT Development</small>
+                            {{-- <a href="/register">Register Now!</a> --}}
+                        </div>
+                    </form>
+                </main>
+            </div>
         </div>
     </div>
 
@@ -82,7 +86,5 @@
     <script src="/js/dashboard.js"></script>
 
 </body>
-
-
 
 </html>
